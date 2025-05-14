dd901e18860d4412af6a3b738625db8cc681a833
Damyan Maxwell <saintnyte@gmail.com>
2025-05-14 14:41:55 +0200

    Done

---
+++ eersteproject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+
+  static void Main(string[] args){
+
+    int Nummer = 7;
+    string Text = "Lucky number 777";
+    bool Fact = true;
+    float AnderNummer = 7;
+
+    Console.WriteLine("Nummer: " + Nummer);
+    Console.WriteLine("Text: " + Text);
+    Console.WriteLine("Fact: " + Fact);
+    Console.WriteLine("Andere Nummer: " + AnderNummer);
+
+
+
+
+  }
+
+}
