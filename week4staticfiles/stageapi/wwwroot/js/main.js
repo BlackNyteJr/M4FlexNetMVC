00275b76ab6a392d7a31220d3dcb26f1efd88f96
Damyan Maxwell <saintnyte@gmail.com>
2025-06-24 19:32:23 +0200

    week4_staticfiles/02 af

---
+++ week4staticfiles/stageapi/wwwroot/js/main.js
@@ -0,0 +1,22 @@
+let stages = document.getElementById("stages");
+
+
+async function getData() {
+  const url = "https://example.org/products.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+.then(response  => response.text())
+.then((text) => {
+    stages.innerHTML = text;
+})
